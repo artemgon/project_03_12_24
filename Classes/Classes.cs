a4d25cff1cde3ddfdfd7844a3eb21a4afa18402a
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 14:44:11 +0200

    Task 3 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,34 @@
 ﻿
 namespace project_03_12_24.Classes
 {
-    internal class Classes
+    public interface ISort
     {
+        void SortAsc();
+        void SortDesc();
+        void SortByParam(bool isAsc);
+    }
+    public class IntArray(int[] array) : ISort
+    {
+        private readonly int[] _array = array;
+        public void SortAsc()
+        {
+            Array.Sort(_array);
+        }
+        public void SortDesc()
+        {
+            Array.Sort(_array);
+            Array.Reverse(_array);
+        }
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc)
+            {
+                SortAsc();
+            }
+            else
+            {
+                SortDesc();
+            }
+        }
     }
 }
