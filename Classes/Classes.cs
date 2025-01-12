153351b9357705fd682f2cb03eed4fcb5215ecf9
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 09:32:23 +0200

    Task 1 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,39 @@
 ﻿
+using System.Collections.Generic;
+
 namespace project_03_12_24.Classes
 {
-    internal class Classes
+    interface ICalc
     {
+        int Less(int valueToCompare); // quantity of elements less than valueToCompare
+        int Greater(int valueToCompare); // quantity of elements greater than valueToCompare
+    }
+    public class IntArray(int[] array) : ICalc
+    {
+        private readonly int[] _array = array;
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+            foreach (var item in _array)
+            {
+                if (item < valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+            foreach (var item in _array)
+            {
+                if (item > valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ namespace project_03_12_24
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
-            { 
+            {
+                int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                ICalc calc = new IntArray(array);
+                Console.WriteLine($"Less than 5: {calc.Less(5)}");
+                Console.WriteLine($"Greater than 5: {calc.Greater(5)}");
+                Console.WriteLine();
             }
             catch (Exception e)
             {
